de6796d0eba60eaa8a08ce0ef0786bff267a1a38
Phillip Kheyfets <phillipkheyfets@gmail.com>
2014-04-14 03:07:20 -0500

    fourth commit

--- js/game.js
+++ js/game.js
@@ -1,4 +1,3 @@
-
 var step = 0;
 
 var descriptions = [];
@@ -10,156 +9,156 @@ descriptions[3] = "Macbeth is celebrating his new title with friends, when he se
 var description = "<span style='color: blue'>Description</span>:<br>";
 
 function action(command) {
-	addText("<span style='color: limeGreen'>" + command + "</span>");
-	resetInput();
-	process(command);
-	scrollDown();
+  addText("<span style='color: limeGreen'>" + command + "</span>");
+  resetInput();
+  process(command);
+  scrollDown();
 }
 
 function process(command) {
-	var kill = command.substring(0, 4) === "kill";
-	var noKill = command === "no-kill";
+  var kill = command.substring(0, 4) === "kill";
+  var noKill = command === "no-kill";
 
-	if (step == 0) {
+  if (step == 0) {
 
-		// If user enters "kill" + character
-		if (kill) {
-			if (command.substring(5).toUpperCase() === "WITCHES") {
-				addText("The Witches die.");
-				addText(description + "Macbeth gets crowned Thane of Cawdor and is congratulated by Banquo. He lives somewhat happily ever after with his annoying wife Lady Macbeth.");
-				$("#game-input").attr("disabled", "true");
-				gameOver();
-				return false;
-			} else if (command = "kill") {
-				addText("Must specify a character. Try again!");
-			} else {
-				addText("Macbeth cannot kill them. Try again!");
-			}
-		}
+    // If user enters "kill" + character
+    if (kill) {
+      if (command.substring(5).toUpperCase() === "WITCHES") {
+        addText("The Witches die.");
+        addText(description + "Macbeth gets crowned Thane of Cawdor and is congratulated by Banquo. He lives somewhat happily ever after with his annoying wife Lady Macbeth.");
+        $("#game-input").attr("disabled", "true");
+        gameOver();
+        return false;
+      } else if (command = "kill") {
+        addText("Must specify a character. Try again!");
+      } else {
+        addText("Macbeth cannot kill them. Try again!");
+      }
+    }
 
-		// If user enters "no-kill"
-		else if (noKill) {
-			addText("The witches share the following prophecies: Macbeth will be Thane of Glamis, Cawdor, and then King of Scotland.");
-			addText(description + descriptions[++step]);
-		}
+    // If user enters "no-kill"
+    else if (noKill) {
+      addText("The witches share the following prophecies: Macbeth will be Thane of Glamis, Cawdor, and then King of Scotland.");
+      addText(description + descriptions[++step]);
+    }
 
-		// If user enters something else
-		else {
-			addText("Not a command. Try again!");
-		}
+    // If user enters something else
+    else {
+      addText("Not a command. Try again!");
+    }
 
-	}
+  }
 
-	else if (step == 1) {
+  else if (step == 1) {
 
-		// If user enters "kill" + character
-		if (kill) {
-			if (command.substring(5).toUpperCase() === "DUNCAN") {
-				addText("Duncan is murdered in his sleep.");
-				addText(description + descriptions[++step]);
-			} else if (command.toUpperCase() === "KILL") {
-				addText("Must specify a character. Try again!");
-			} else {
-				addText("Macbeth cannot kill them. Try again!");
-			}
-		}
+    // If user enters "kill" + character
+    if (kill) {
+      if (command.substring(5).toUpperCase() === "DUNCAN") {
+        addText("Duncan is murdered in his sleep.");
+        addText(description + descriptions[++step]);
+      } else if (command.toUpperCase() === "KILL") {
+        addText("Must specify a character. Try again!");
+      } else {
+        addText("Macbeth cannot kill them. Try again!");
+      }
+    }
 
-		// If user enters "no-kill"
-		else if (noKill) {
-			addText("Duncan is murdered in his sleep by Lady Macbeth.");
-			addText(description + descriptions[++step]);
-		}
+    // If user enters "no-kill"
+    else if (noKill) {
+      addText("Duncan is murdered in his sleep by Lady Macbeth.");
+      addText(description + descriptions[++step]);
+    }
 
-		// If user enters something else
-		else {
-			addText("Not a command. Try again!");
-		}
+    // If user enters something else
+    else {
+      addText("Not a command. Try again!");
+    }
 
-	}
+  }
 
-	else if (step == 2) {
+  else if (step == 2) {
 
-		// If user enters "kill" + character
-		if (kill) {
-			if (command.substring(5).toUpperCase() === "BANQUO") {
-				addText("Banquo is killed by murderers, but his son, Fleance, escapes.");
-				addText(description + descriptions[++step]);
-			} else if (command.toUpperCase() === "KILL") {
-				addText("Must specify a character. Try again!");
-			} else {
-				addText("Macbeth cannot kill them. Try again!");
-			}
-		}
+    // If user enters "kill" + character
+    if (kill) {
+      if (command.substring(5).toUpperCase() === "BANQUO") {
+        addText("Banquo is killed by murderers, but his son, Fleance, escapes.");
+        addText(description + descriptions[++step]);
+      } else if (command.toUpperCase() === "KILL") {
+        addText("Must specify a character. Try again!");
+      } else {
+        addText("Macbeth cannot kill them. Try again!");
+      }
+    }
 
-		// If user enters "no-kill"
-		else if (noKill) {
-			addText("Lady Macbeth sends the murderers and Banquo is killed by them, but his son, Fleance, escapes.");
-			addText(description + descriptions[++step]);
-		}
+    // If user enters "no-kill"
+    else if (noKill) {
+      addText("Lady Macbeth sends the murderers and Banquo is killed by them, but his son, Fleance, escapes.");
+      addText(description + descriptions[++step]);
+    }
 
-		// If user enters something else
-		else {
-			addText("Not a command. Try again!");
-		}
+    // If user enters something else
+    else {
+      addText("Not a command. Try again!");
+    }
 
-	}
+  }
 
-	else if (step == 3) {
+  else if (step == 3) {
 
-		// If user enters "kill" + character
-		if (kill) {
-			if (command.substring(5).toUpperCase() === "MACDUFF") {
-				addText("Macbeth doesn't successfully kill Macduff. Instead Macduff, who was a C-section, kills Macbeth.");
-				gameOver();
-				return false;
-			} else if (command.toUpperCase() === "KILL") {
-				addText("Must specify a character. Try again!");
-			} else {
-				addText("Macbeth cannot kill them. Try again!");
-			}
-		}
+    // If user enters "kill" + character
+    if (kill) {
+      if (command.substring(5).toUpperCase() === "MACDUFF") {
+        addText("Macbeth doesn't successfully kill Macduff. Instead Macduff, who was a C-section, kills Macbeth.");
+        gameOver();
+        return false;
+      } else if (command.toUpperCase() === "KILL") {
+        addText("Must specify a character. Try again!");
+      } else {
+        addText("Macbeth cannot kill them. Try again!");
+      }
+    }
 
-		// If user enters "no-kill"
-		else if (noKill) {
-			addText("Macduff, who was a C-section, kills Macbeth.");
-			gameOver();
-			return false;
-		}
+    // If user enters "no-kill"
+    else if (noKill) {
+      addText("Macduff, who was a C-section, kills Macbeth.");
+      gameOver();
+      return false;
+    }
 
-		// If user enters something else
-		else {
-			addText("Not a command. Try again!");
-		}
+    // If user enters something else
+    else {
+      addText("Not a command. Try again!");
+    }
 
-	}
+  }
 
 }
 
 function addText(text) {
-	document.getElementById("game-text").innerHTML += "<br>" + text;
+  document.getElementById("game-text").innerHTML += "<br>" + text;
 }
 
 function resetInput() {
-	document.getElementById("game-input").value = "";
+  document.getElementById("game-input").value = "";
 }
 
 function scrollDown() {
-	var el = $("#game-text");
+  var el = $("#game-text");
   var height = el[0].scrollHeight;
   el.scrollTop(height);
 }
 
 function getLastLine() {
-	return lastLine = document.getElementById("game-text").innerHTML.substring(document.getElementById("game-text").innerHTML.lastIndexOf("<br>") + 4);
+  return lastLine = document.getElementById("game-text").innerHTML.substring(document.getElementById("game-text").innerHTML.lastIndexOf("<br>") + 4);
 }
 
 function gameOver() {
-	addText("<span style='font-size: 18px; font-weight: bold'>Game over...</span><br><button class='btn btn-large btn-submit' onclick='resetGame()'>Play again</button>");
+  addText("<span style='font-size: 18px; font-weight: bold'>Game over...</span><br><button class='btn btn-large btn-submit' onclick='resetGame()'>Play again</button>");
 }
 
 function resetGame() {
-	document.getElementById("game-text").innerHTML = description + descriptions[0];
-	$("#game-input").removeAttr("disabled");
-	step = 0;
-	characters = restoreCharacters;
+  document.getElementById("game-text").innerHTML = description + descriptions[0];
+  $("#game-input").removeAttr("disabled");
+  step = 0;
+  characters = restoreCharacters;
 }
